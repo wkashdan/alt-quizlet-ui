7c4dda9a17a96d12501f5dfda946723a0d01ac48
Will Kashdan <wgkashdan@gmail.com>
2017-02-05 19:56:40 -0500

    Abstracted aq-menu to accept an attribute that will be displayed

--- app/components/altquiz-set-menu.js
+++ app/components/altquiz-set-menu.js
@@ -1,10 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  classNames: ['altquiz-set-menu'],
-  tagName: 'ul',
-
-  click(event) {
-    console.log(this.get('sets'));
-  }
 });
--- app/components/aq-menu.js
+++ app/components/aq-menu.js
@@ -1,4 +1,25 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
+  //====================================
+  // Style Properties
+  //====================================
+
+  classNames: ['aq-menu'],
+  tagName: 'ul',
+
+  //====================================
+  // Required Properties
+  //====================================
+
+  items: null,
+  displayAttrName: null,
+  itemsWDisplayAttrName: Ember.computed('displayAttrName', 'items.[]', function() {
+    return this.get('items').map((item) => {
+      let attrName = this.get('displayAttrName');
+      item.set('displayValue', item.get(attrName));
+      return item;
+    });
+  })
 });
